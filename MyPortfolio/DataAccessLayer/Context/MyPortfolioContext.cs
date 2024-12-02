fe7ec752ff0bdaf461ee0033e9ba074c0e260cf2
berkay <aberkaysatilmis@gmail.com>
2024-12-02 19:50:59 +0300

    Viewler Eklendi Düzenlemeler Yapıldı

---
+++ MyPortfolio/DataAccessLayer/Context/MyPortfolioContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolio.DataAccessLayer.Entities;
+
+namespace MyPortfolio.DataAccessLayer.Context
+{
+    public class MyPortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-3F18H4A\\MSSQLSERVERR;initial Catalog=MyPortfolioDb;integrated Security=true;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
