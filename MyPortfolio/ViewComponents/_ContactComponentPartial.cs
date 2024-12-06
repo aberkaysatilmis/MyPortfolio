e9e6ca164ee4ea09fcb3dc2e75ecbecbb23caf7e
berkay <aberkaysatilmis@gmail.com>
2024-12-06 14:30:10 +0300

    Proje Sonu

--- MyPortfolio/ViewComponents/_ContactComponentPartial.cs
+++ MyPortfolio/ViewComponents/_ContactComponentPartial.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DataAccessLayer.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _ContactComponentPartial : ViewComponent
     {
+        MyPortfolioContext context = new MyPortfolioContext();
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var value = context.Contacts.ToList();
+
+            return View(value);
         }
     }
 }
